975d8ebf1c7b336cf2c7ad414992784fcaa3c3b5
GagliardeNicolapio <gagliardenicolapio00@gmail.com>
2022-03-24 11:31:31 +0100

    AND e OR aggregation di J48, Naive Bayes, SVM e Logicistic Regression

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 
+
 # stampa nomaColonna->numValMancanti
 def missing_values(df):
     print("Valori mancanti")
@@ -88,6 +89,36 @@ def print_metrics(confusion_matrix):
     print("False positive rate: ", fp / (fp + tn))
 
 
+def aggregation(application_pred, network_pred, or_flag=True, and_flag=True):
+    if len(application_pred) != len(network_pred):
+        raise Exception("application length != network length")
+
+    if or_flag & and_flag: # entrambi
+        aggregation_or = np.array([])
+        for i in range(len(application_pred)):
+            aggregation_or = np.append(aggregation_or, application_pred[i] or network_pred[i])
+
+        aggregation_and = np.array([])
+        for i in range(len(application_pred)):
+            aggregation_and = np.append(aggregation_and, application_pred[i] or network_pred[i])
+
+        return aggregation_or, aggregation_and
+
+    elif or_flag & (not and_flag): # solo or aggregation
+        aggregation_or = np.array([])
+        for i in range(len(application_pred)):
+            aggregation_or = np.append(aggregation_or, application_pred[i] or network_pred[i])
+
+        return aggregation_or
+
+    elif (not or_flag) &  and_flag: # solo and aggregation
+        aggregation_and = np.array([])
+        for i in range(len(application_pred)):
+            aggregation_and = np.append(aggregation_and, application_pred[i] or network_pred[i])
+
+        return aggregation_and
+
+
 if __name__ == "__main__":
     df = pd.read_csv('./dataset/dataset.csv')
 
@@ -154,31 +185,79 @@ if __name__ == "__main__":
     plt.show()
     smote = SMOTE()
     X, y = smote.fit_resample(df[df.columns.values.tolist()[:-1]], df['Type'])
-    #df = pd.DataFrame(X, columns=df.columns.values.tolist()[:-1])
+    # df = pd.DataFrame(X, columns=df.columns.values.tolist()[:-1])
 
     # sns.countplot(x="Type", data=df)
     # plt.show()
 
     # J48 data-aggregation
-    print("j48 " + "-" * 60)
-    j48 = DecisionTreeClassifier()
-    j48_pred = cross_val_predict(j48, X, y, cv=10)
-    print_metrics(confusion_matrix(y, j48_pred))
+    print("j48 data-aggregation " + "-" * 60)
+    j48_data_agg = DecisionTreeClassifier()
+    j48_pred_data_agg = cross_val_predict(j48_data_agg, X, y, cv=10)
+    print_metrics(confusion_matrix(y, j48_pred_data_agg))
 
     # Naive Bayes data-aggregation
-    print("Naive Bayes " + "-" * 60)
-    nby = GaussianNB() # valori continui
+    print("Naive Bayes data-aggregation " + "-" * 60)
+    nby = GaussianNB()  # valori continui
     nby_pred = cross_val_predict(nby, X, y, cv=10)
     print_metrics(confusion_matrix(y, nby_pred))
 
     # SVM data-aggregation
-    print("Support Vector Machine " + "-" * 60)
-    svm = svm.SVC()
-    svm_pred = cross_val_predict(svm, X, y, cv=10)
+    print("Support Vector Machine data-aggregation" + "-" * 60)
+    svm_al = svm.SVC()
+    svm_pred = cross_val_predict(svm_al, X, y, cv=10)
     print_metrics(confusion_matrix(y, svm_pred))
 
     # Logicistic data-aggregation
-    print("Logicistic " + "-" * 60)
+    print("Logicistic data-aggregation" + "-" * 60)
     lcs = LogisticRegression()
     lcs_pred = cross_val_predict(lcs, X, y, cv=10)
     print_metrics(confusion_matrix(y, lcs_pred))
+
+    # split dataset per OR, AND e XOR aggregation
+    application_df = X[
+        ['URL_LENGTH', 'NUMBER_SPECIAL_CHARACTERS', 'CHARSET', 'SERVER', 'CONTENT_LENGTH', 'WHOIS_COUNTRY',
+         'WHOIS_STATEPRO', 'WHOIS_REGDATE', 'WHOIS_UPDATED_DATE', 'DNS_QUERY_TIMES']]
+    network_df = X[
+        ['TCP_CONVERSATION_EXCHANGE', 'DIST_REMOTE_TCP_PORT', 'REMOTE_IPS', 'APP_BYTES', 'SOURCE_APP_PACKETS',
+         'REMOTE_APP_PACKETS', 'SOURCE_APP_BYTES', 'REMOTE_APP_BYTES', 'APP_PACKETS']]
+
+    # J48 OR e AND-aggregation
+    print("J48 OR e AND-aggregation " + "-" * 60)
+    j48_pred_or_agg_application = cross_val_predict(DecisionTreeClassifier(), application_df, y, cv=10)
+    j48_pred_or_agg_network = cross_val_predict(DecisionTreeClassifier(), network_df, y, cv=10)
+    aggregation_or, aggregation_and = aggregation(j48_pred_or_agg_application, j48_pred_or_agg_network)
+    print("J48 OR-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_or))
+    print("J48 AND-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_and))
+
+    # Naive Bayes OR e AND-aggregation
+    print("Naive Bayes OR e AND-aggregation " + "-" * 60)
+    nby_pred_or_agg_application = cross_val_predict(GaussianNB(), application_df, y, cv=10)
+    nby_pred_or_agg_network = cross_val_predict(GaussianNB(), network_df, y, cv=10)
+    aggregation_or, aggregation_and = aggregation(nby_pred_or_agg_application, nby_pred_or_agg_network)
+    print("Naive Bayes OR-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_or))
+    print("Naive Bayes AND-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_and))
+
+    # SVM OR e AND-aggregation
+    print("Support vector machine OR e AND-aggregation " + "-" * 60)
+    svm_pred_or_agg_application = cross_val_predict(svm.SVC(), application_df, y, cv=10)
+    svm_pred_or_agg_network = cross_val_predict(svm.SVC(), network_df, y, cv=10)
+    aggregation_or, aggregation_and = aggregation(svm_pred_or_agg_application, svm_pred_or_agg_network)
+    print("Support vector machine OR-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_or))
+    print("Support vector machine AND-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_and))
+
+    # Logicistic OR e AND-aggregation
+    print("Logicistic OR e AND-aggregation " + "-" * 60)
+    lcs_pred_or_agg_application = cross_val_predict(LogisticRegression(), application_df, y, cv=10)
+    lcs_pred_or_agg_network = cross_val_predict(LogisticRegression(), network_df, y, cv=10)
+    aggregation_or, aggregation_and = aggregation(lcs_pred_or_agg_application, lcs_pred_or_agg_network)
+    print("Logicistic OR-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_or))
+    print("Logicistic AND-aggregation " + "-" * 10)
+    print_metrics(confusion_matrix(y, aggregation_and))
